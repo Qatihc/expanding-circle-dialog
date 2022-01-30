18c53da1991c127262cd6f4241858b508eacaa24
Matias <matias_casali@hotmail.com>
2022-01-30 20:39:08 -0300

    Commit inicial.

---
+++ src/CircleModal.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const CircleModal = ({ className }) => {
+  return (
+    <p className={className}>
+      asd
+    </p>
+  )
+}
+
+export default styled(CircleModal)`
+  color: red;
+`;
